530b9b3952b5c6450ae8c046fe90fa4d10536696
nnngu <654015592@qq.com>
2018-02-03 11:57:50 +0800

    提交代码

---
+++ src/main.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+import re
+import requests
+
+
+def dowmloadPic(html, keyword):
+    pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
+    i = 1
+    print('找到关键词:' + keyword + '的图片，现在开始下载图片...')
+    for each in pic_url:
+        print('正在下载第' + str(i) + '张图片，图片地址:' + str(each))
+        try:
+            pic = requests.get(each, timeout=10)
+        except requests.exceptions.ConnectionError:
+            print('【错误】当前图片无法下载')
+            continue
+
+        dir = '../images/' + keyword + '_' + str(i) + '.jpg'
+        fp = open(dir, 'wb')
+        fp.write(pic.content)
+        fp.close()
+        i += 1
+
+
+if __name__ == '__main__':
+    word = input("Input key word: ")
+    url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + word + '&ct=201326592&v=flip'
+    result = requests.get(url)
+    dowmloadPic(result.text, word)
